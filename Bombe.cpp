2683ef83293ba961ad141f66e1b46766ff330802
akorba <anna.korba@ensae.fr>
2014-02-21 20:40:08 +0100

    Update Bombe.cpp

--- Bombe.cpp
+++ Bombe.cpp
@@ -15,22 +15,23 @@ using namespace std;
 
 const int BlockSize=70;
 const int BlockSize2=48;
- 
+
 Bombe::Bombe() : m_degats(10), m_taille(1), m_type("type 1")
 {
- 
+
 }
- 
+
 Bombe::Bombe(int degats, int taille, string type) : m_degats(degats), m_taille(taille), m_type(type)
 {
- 
+
 }
 
 
 Image image2;
 
 
-void Bombe::poser(RenderWindow &Window, string nomimage, int i, int j, Sprite TableauSprites[10][10], int MapFile[10][10] ) 
+
+void Bombe::exploser(RenderWindow &Window, string nomimage, int i, int j, Sprite TableauSprites[10][10], int MapFile[10][10] ) 
 {
 	if(!image2.LoadFromFile(nomimage))
 		{
@@ -46,22 +47,22 @@ void Bombe::poser(RenderWindow &Window, string nomimage, int i, int j, Sprite Ta
 			Bombe.Resize(BlockSize2,BlockSize2);
 			TableauSprites[i][j]=Bombe;
 			TableauSprites[i][j].SetPosition(i*BlockSize2, j*BlockSize2);
+
 			DrawMap(Window, TableauSprites);
-	}
-}
-
-void Bombe::exploser(RenderWindow &Window, string nomimage, int i, int j, Sprite TableauSprites[10][10], int MapFile[10][10] ) 
-{
-	if(!image2.LoadFromFile(nomimage))
-		{
-			cout << "Erreur chargement image" << endl;
-		}
-
-		else
-		{	
 
 // elle va maintenant détruire ce qui l'entoure:
-			
+			Clock clock;
+			float elapsedtime=0;/*
+			float elapsedtime2=0;*/
+
+			while (elapsedtime<0.1)
+			{
+				elapsedtime=clock.GetElapsedTime()+Window.GetFrameTime();
+			}
+
+			if (elapsedtime >= 0.1)
+			{
+
 					Sprite Casse;
 					Casse.SetImage(image2);
 					Casse.SetSubRect(IntRect(2*BlockSize , 2*BlockSize, 3*BlockSize, 3*BlockSize)); 
@@ -84,19 +85,25 @@ void Bombe::exploser(RenderWindow &Window, string nomimage, int i, int j, Sprite
 
 
 					DrawMap(Window, TableauSprites);
-
-// puis les briques vont disparaître: 
-
-			/*Clock clockbombe;
-			float elapsedtime2=0;
-
-			while (elapsedtime2<50*Window.GetFrameTime())
-			{
-				elapsedtime2=clockbombe.GetElapsedTime()+Window.GetFrameTime();
 			}
 
-			if (elapsedtime2 >= 50* Window.GetFrameTime())
+
+// désormais on voudrait que les briques cassées disparaissent et laissent place à de l'herbe.
+// de plus elle deviendraient traversables grâce à "MapFile [i][j]=0".
+// seulement cela fait bugger le jeu.
+// nous vous laissons la deuxieme étape en commentaire (avec float elapsedtime2 au début et la boucle ci-dessous) 
+// si vous l'éxécutez vous verrez à peu près à quoi cela aurait pu ressembler (d'abord on affiche la bombe puis les sprites autour changent)
+
+
+
+			/*while (elapsedtime2<30*Window.GetFrameTime())
 			{
+				elapsedtime2=clock.GetElapsedTime()+Window.GetFrameTime();
+			}
+
+			if (elapsedtime2>=30*Window.GetFrameTime())
+			{
+
 				Sprite Herbe;
 				Herbe.SetImage(image2);
 				Herbe.SetSubRect(IntRect(2*BlockSize , 0, 3*BlockSize, BlockSize)); 
@@ -124,11 +131,14 @@ void Bombe::exploser(RenderWindow &Window, string nomimage, int i, int j, Sprite
 				MapFile[i][j-1]=0;
 
 				DrawMap(Window, TableauSprites);
-			}
+				}*/
 
-			clockbombe.Reset();*/
-	}
+			clock.Reset();
+
+		}
+
 }
+
 
 
 
